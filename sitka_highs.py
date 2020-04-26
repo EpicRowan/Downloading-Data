9ef7d2bade6ee8256d25b2ed1b2291cdb2765622
Rowan Shepherd <shepherd.rowan.k@gmail.com>
2020-04-26 13:54:11 -0700

    imported matplotlib to chart high temperatures in a graph

--- sitka_highs.py
+++ sitka_highs.py
@@ -1,5 +1,7 @@
 import csv
 
+import matplotlib.pyplot as plt
+
 filename = 'data/sitka_weather_07-2018_simple.csv'
 
 with open(filename) as f:
@@ -7,5 +9,21 @@ with open(filename) as f:
 	header_row = next(reader)
 	print(header_row)
 
-for index, column_header in enumerate(header_row):
-	print(index, column_header)
+# Get high temperature from this file
+	highs = []
+	for row in reader:
+		high= int(row[5])
+		highs.append(high)
+
+# Plot the high temperatures
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.plot(highs, c='red')
+
+# Format plot
+ax.set_title("Daily high temperatures, July 2019", fontsize = 24)
+ax.set_xlabel('', fontsize = 16)
+ax.set_ylabel("Temperature(F)", fontsize = 16)
+ax.tick_params(axis='both', which = 'major', labelsize=16)
+
+plt.show()
